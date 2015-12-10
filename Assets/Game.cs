ed48946db443ba5d71f14253355a4e081a6ce5ac
alycen <alsunnid@gmail.com>
2015-12-10 14:44:01 +0000

    first commit

---
+++ Assets/Game.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Instantiate(playerPrefab, new Vector3(-7,0,0), new Quaternion());
+        //Network.Instantiate(playerPrefab, new Vector3(0,0,0), new Quaternion(), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Transform thisTransform;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        thisTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (GetComponent<NetworkView>().isMine)
+        //{
+            InputMovement();
+        //}
+	}
+
+    private void InputMovement()
+    {
+        Vector2 pos = thisTransform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed;
+        }
+
+        thisTransform.position = pos;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("CAUGHT");
+    }
+}
